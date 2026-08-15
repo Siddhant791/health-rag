f313b591779fbf76ab0ebc9ff130a884a7d78f2c
siddhantbhardwaj <bhardwajsiddhant6@gmail.com>
2026-08-15 19:22:58 +0530

    Implemented storing file

--- src/main/java/com/siddhant/healthreport/medicalaibot/report/service/LocalStorageService.java
+++ src/main/java/com/siddhant/healthreport/medicalaibot/report/service/LocalStorageService.java
@@ -8,9 +8,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 import java.util.UUID;
 
@@ -29,7 +34,7 @@ public class LocalStorageService implements StorageService {
     }
 
     @Override
-    public StoredFile store(MultipartFile file) {
+    public StoredFile store(MultipartFile file) throws NoSuchAlgorithmException, IOException {
         // Extract original file name
         String originalFileName = file.getOriginalFilename();
 
@@ -42,11 +47,32 @@ public class LocalStorageService implements StorageService {
         // Resolve storage path with root path
         Path destination = storagePath.resolve(storedFileName);
 
+        // Final destination where file needs to be stored
         destination = destination.normalize();
 
         if (!destination.startsWith(storagePath)) {
             throw new RuntimeException("Invalid storage Path");
         }
+
+        // Let's read the file now and store it in the destination
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+        // Output stream
+        OutputStream outputStream = Files.newOutputStream(destination);
+
+        InputStream inputFileStream = file.getInputStream();
+
+        DigestInputStream digestInputStream = new DigestInputStream(inputFileStream,digest);
+
+        // 8KB of buffer
+        byte[] buffer = new byte[8192];
+
+        int byteRead;
+
+        while((byteRead = digestInputStream.read(buffer)) != -1){
+            outputStream.write(buffer,0,byteRead);
+        }
+
 
         return null;
     }
--- src/main/java/com/siddhant/healthreport/medicalaibot/report/storage/StorageService.java
+++ src/main/java/com/siddhant/healthreport/medicalaibot/report/storage/StorageService.java
@@ -2,6 +2,9 @@ package com.siddhant.healthreport.medicalaibot.report.storage;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
 public interface StorageService {
-    StoredFile store(MultipartFile file);
+    StoredFile store(MultipartFile file) throws NoSuchAlgorithmException,IOException;
 }
